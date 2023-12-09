81a5b0dba44bc160470f135ba1dda91d6240fe76
WilliamNunes905 <williamtrybe@gmail.com>
2023-12-09 16:25:10 -0300

    feat: Adding the badge to the button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,13 +3,23 @@ import {
   faCartFlatbedSuitcase,
 } from '@fortawesome/free-solid-svg-icons/faCartFlatbedSuitcase';
 import './ReservationButton.scss';
+import { Badge, Avatar } from 'antd';
 
 function ReservationButton() {
   return (
-    <button>
-      <FontAwesomeIcon icon={ faCartFlatbedSuitcase } />
-      Reservas
-    </button>
+    <>
+      <button>
+        <FontAwesomeIcon icon={ faCartFlatbedSuitcase } />
+        Reservas
+      </button>
+      <div className="container-badge">
+        <Badge
+          size="default"
+          count={ 5 }
+          className="badge-reservation"
+        />
+      </div>
+    </>
   );
 }
 

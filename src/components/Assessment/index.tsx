020f5c84f881d506b621a978f696d7808365b630
WilliamNunes905 <williamtrybe@gmail.com>
2023-12-16 22:48:19 -0300

    feat: install Icon AntDesigner

---
+++ src/components/Assessment/index.tsx
@@ -0,0 +1,19 @@
+import { UserOutlined } from '@ant-design/icons';
+import './Assessment.scss';
+
+export function Assessment() {
+  return (
+    <div className="container-assessment">
+      <div className="frame-113">
+        <div className="frame-115">
+          <h1>Avaliações</h1>
+          <div className="frame-110">
+            <button>asdasd</button>
+            <UserOutlined />
+            <button>asdasdasasdasd</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

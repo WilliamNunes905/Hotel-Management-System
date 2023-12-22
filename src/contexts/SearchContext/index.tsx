8bb2261e7273679813ca6986514a9a74b3ebb474
WilliamNunes905 <williamtrybe@gmail.com>
2023-12-21 21:52:52 -0300

    fix: adjusting state SearchContext

--- src/contexts/SearchContext/index.tsx
+++ src/contexts/SearchContext/index.tsx
@@ -1,41 +1,24 @@
 import { createContext, useState, Dispatch, SetStateAction } from 'react';
-import { Hospedes, HotelStay } from '../../types/HospedesType';
+import { Hospedes } from '../../types/HospedesType';
 
 type SearchContextValue = {
   hotelGuests: Hospedes;
   setHotelGuests: Dispatch<SetStateAction<Hospedes>>;
-  hotelStay: HotelStay;
-  setHotelStay: Dispatch<SetStateAction<HotelStay>>;
 };
 
-const initialValue: SearchContextValue = {
-  hotelGuests: {
-    adults: '1',
-    child: '1',
-  },
-  setHotelGuests: () => {},
-  hotelStay: {
-    entry: '',
-    exit: '',
-  },
-  setHotelStay: () => {},
-};
-
-export const SearchContext = createContext(initialValue);
+export const SearchContext = createContext({} as SearchContextValue);
 
 export function SearchProvider({ children } : { children: React.ReactNode }) {
   const [hotelGuests, setHotelGuests] = useState<Hospedes>({
     adults: '1',
     child: '1',
-  });
-  const [hotelStay, setHotelStay] = useState<HotelStay>({
     entry: '',
     exit: '',
   });
 
   return (
     <SearchContext.Provider
-      value={ { hotelGuests, setHotelGuests, hotelStay, setHotelStay } }
+      value={ { hotelGuests, setHotelGuests } }
     >
       { children }
     </SearchContext.Provider>

39c43ed13064e676fc5860c1eff1caad351e6af3
WilliamNunes905 <williamtrybe@gmail.com>
2023-12-22 14:13:22 -0300

    fix: Adjustment searchContext

--- src/contexts/ApartmentContext/index.tsx
+++ src/contexts/ApartmentContext/index.tsx
@@ -4,22 +4,15 @@ import { Quarto } from '../../types/ApartmentListType';
 type SearchContextValue = {
   apartmentList: Quarto[] | null;
   setApartmentList: Dispatch<SetStateAction<Quarto[] | null>>;
-  bedrooms: string[];
-  setBedrooms: Dispatch<SetStateAction<string[]>>;
+  bedrooms: Quarto[];
+  setBedrooms: Dispatch<SetStateAction<Quarto[]>>;
 };
 
-const initialValue: SearchContextValue = {
-  apartmentList: null,
-  setApartmentList: () => {},
-  bedrooms: [],
-  setBedrooms: () => {},
-};
-
-export const ApartmentContext = createContext(initialValue);
+export const ApartmentContext = createContext({} as SearchContextValue);
 
 export function ApartmentProvider({ children }: { children: React.ReactNode }) {
   const [apartmentList, setApartmentList] = useState<Quarto[] | null>(null);
-  const [bedrooms, setBedrooms] = useState<string[]>([]);
+  const [bedrooms, setBedrooms] = useState<Quarto[]>([]);
 
   return (
     <ApartmentContext.Provider

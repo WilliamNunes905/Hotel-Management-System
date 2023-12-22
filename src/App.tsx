4c0b1185499517119e4ceadf95ab5727ccb090cf
WilliamNunes905 <williamtrybe@gmail.com>
2023-12-21 21:42:05 -0300

    fix: adjusting  providers file

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { SearchProvider } from './contexts/SearchContext';
-import { ApartmentProvider } from './contexts/ApartmentContext';
-import { PaymentsProvider } from './contexts/PaymentsContext';
 import { Payments } from './components/Payments';
 import { NotFound } from './components/PageNotFound';
 
 function App() {
   return (
-    <PaymentsProvider>
-      <ApartmentProvider>
-        <SearchProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={ <Home /> } />
-            <Route path="/payment" element={ <Payments /> } />
-            <Route path="*" element={ <NotFound /> } />
-          </Routes>
-          <Footer />
-        </SearchProvider>
-      </ApartmentProvider>
-    </PaymentsProvider>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/payment" element={ <Payments /> } />
+        <Route path="*" element={ <NotFound /> } />
+      </Routes>
+      <Footer />
+    </div>
+
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import { ApartmentProvider } from './contexts/ApartmentContext';
+import { PaymentsProvider } from './contexts/PaymentsContext';
+import { SearchProvider } from './contexts/SearchContext';
 import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <ApartmentProvider>
+      <PaymentsProvider>
+        <SearchProvider>
+          <App />
+        </SearchProvider>
+      </PaymentsProvider>
+    </ApartmentProvider>
   </BrowserRouter>,
 );
